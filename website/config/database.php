b1a9669aa3507ac778d8be91630803ad210baebd
sujitamainali1 <132050476+sujitamainali1@users.noreply.github.com>
2024-01-31 15:35:52 +0545

    Update database.php

--- website/config/database.php
+++ website/config/database.php
@@ -1,1 +1,17 @@
+<?php
 
+    $serverName = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'garden';
+
+    $connection = mysqli_connect($serverName,$username,$password,$database);
+
+    if(!$connection){
+        echo "<br> Connection failed!";
+        die (mysqli_connect_error());
+    }
+    else{
+        echo "Connection Successful";
+    }
+?>
