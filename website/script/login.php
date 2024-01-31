d3c155808082f09c03a2a9681ef2f110438c72b9
sujitamainali1 <132050476+sujitamainali1@users.noreply.github.com>
2024-01-31 15:38:08 +0545

    Update login.php

--- website/script/login.php
+++ website/script/login.php
@@ -17,23 +17,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
  
     $UserControllers= new UserControllers();
     $UserControllers -> login($email, $password);
-
-    // if ($co == 1) {
-    //     $login = true;
-    // } else {  
-    
-    //     echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    //     Credentials did not match. <strong>Please try again</strong>
-    //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    //         <span aria-hidden="true"></span>
-    //     </button>
-    // </div>';
-    // }
-
-    // if ($login) {
-    //     // Redirect to the welcome page after successful login
-    //     header("location: ../template/user.html");
-    //     exit();
-    // }
-}
 ?>
